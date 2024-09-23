cfcb188f5022ae6df868786c890fd05ef807bb13
SirTonySama <90422210+SirTonySama@users.noreply.github.com>
2024-09-23 13:36:39 -0400

    Integrating stuff

--- Website/exercise_landing.py
+++ Website/exercise_landing.py
@@ -3,31 +3,12 @@ from flask_login import login_user, login_required, logout_user, current_user, l
 from . import db
 import requests
 
-
-
 exercise_landing = Blueprint('exercise_landing', __name__)
 
-def send_script_request_to_pi(script_name):
-    url = "http://10.0.0.159:5000/run_script"
-    #TODO: Get fields from the front end through user input.
-    #TODO: Integrate different payload if split is launched instead of just one exercise.
-    payload = {
-        "exerciseList": [],
-        "script_name": script_name,
-        "delay": 4, #s
-        "time": 10, #s
-        "animate": False, #default,
-    }
-    response = requests.post(url, json= payload)
-    #if response.status_code == 200:
-    #    return response.json().get("output", "No Output")
-    #else:
-    #    return response.json().get("output", "No Error")
-    
 @exercise_landing.route("/launch-exercise", methods=['GET', 'POST'])
 def exercise_landing_page():
-    if request.method == 'POST':
-        exercise_name = request.form['exercise_name']
-        send_script_request_to_pi(exercise_name)
     return render_template('exercise_landing.html')
+
+
+
 
